d0d7a3f58546cad97f569d75b565a3e4bb9d3f94
shamim hossain <shshamim@aci-bd.com>
2018-03-12 15:19:09 +0600

    Kadane's Algorithm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+int getMax(int a, int b)
+{
+    return a > b ? a : b;
+}
+int maxSum(int a[1000], int n)
+{
+    int m = a[0];
+
+    //BETTER SOLUTION O(n)
+    int lm = m;
+    for (int i = 1; i < n; i++)
+    {
+        lm = getMax(lm + a[i], a[i]);
+        m = getMax(m, lm);
+    }
+
+    //FIRST SOLUTION O(n*n)
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int lm = a[i];
+    //     int temp = lm;
+    //     for (int j = i+1; j < n; j++)
+    //     {
+    //         temp += a[j];
+    //         lm = getMax(temp, lm);
+    //     }
+    //     m = getMax(m, lm);
+    // }
+    return m;
+}
+
 int main()
 {
     int t, n, i, a[1000];
@@ -11,58 +42,8 @@ int main()
         cin >> n;
         for (i = 0; i < n; i++)
             cin >> a[i];
-            
-        //BETTER SOLUTION
-        i = 0;
-        int m = a[i];
-        while (i < n)
-        {
-            int lm = a[i];
-            int temp = lm;
-            for (int j = i + 1; j < n; j++)
-            {
-                temp += a[j];
-                if (temp > lm)
-                {
-                    lm = temp;
-                }
-            }
 
-            if (lm > m)
-            {
-                m = lm;
-                i++;
-            } else {
-                i++;
-            }
-        }
-        cout << m << endl;
-
-        //FIRST SOLUTION
-        // i = 0;
-        // int m = a[i];
-        // while (i < n)
-        // {
-        //     int lm = a[i];
-        //     int temp = lm;
-        //     for (int j = i + 1; j < n; j++)
-        //     {
-        //         temp += a[j];
-        //         if (temp > lm)
-        //         {
-        //             lm = temp;
-        //         }
-        //     }
-
-        //     if (lm > m)
-        //     {
-        //         m = lm;
-        //         i++;
-        //     } else {
-        //         i++;
-        //     }
-        // }
-        // cout << m << endl;
+        cout << maxSum(a, n) << endl;
     }
 
     return 0;
