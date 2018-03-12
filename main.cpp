708f8252d8771ef627cfdb671e01aacc25f707f8
shamim reza <reza_cse08@hotmail.com>
2018-03-13 00:49:39 +0600

    missing Value In Array

--- main.cpp
+++ main.cpp
@@ -2,48 +2,17 @@
 
 using namespace std;
 
-int getMax(int a, int b)
-{
-    return a > b ? a : b;
-}
-int maxSum(int a[1000], int n)
-{
-    int m = a[0];
-
-    //BETTER SOLUTION O(n)
-    int lm = m;
-    for (int i = 1; i < n; i++)
-    {
-        lm = getMax(lm + a[i], a[i]);
-        m = getMax(m, lm);
-    }
-
-    //FIRST SOLUTION O(n*n)
-    // for (int i = 0; i < n; i++)
-    // {
-    //     int lm = a[i];
-    //     int temp = lm;
-    //     for (int j = i+1; j < n; j++)
-    //     {
-    //         temp += a[j];
-    //         lm = getMax(temp, lm);
-    //     }
-    //     m = getMax(m, lm);
-    // }
-    return m;
-}
-
 int main()
 {
-    int t, n, i, a[1000];
+    int t, n, i, a[1001];
     cin >> t;
     while (t--)
     {
         cin >> n;
-        for (i = 0; i < n; i++)
+        for (i = 1; i < n; i++)
             cin >> a[i];
 
-        cout << maxSum(a, n) << endl;
+        cout << missingValueInArray(a, n) << endl;
     }
 
     return 0;
