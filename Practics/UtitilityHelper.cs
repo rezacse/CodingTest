15e48b132181174dde97a742854b7c6e8a0258aa
shamim reza <reza_cse08@hotmail.com>
2020-10-28 22:07:30 +0600

    iterative tree traverse

---
+++ Practics/UtitilityHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Practics
+{
+    public class UtitilityHelper
+    {
+
+        public int[] ConvertToInt(string orderIDs)
+        {
+            return orderIDs.Split("0", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(c => Convert.ToInt32(c)).ToArray();
+        }
+    }
+}
