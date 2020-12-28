35e1bf9ad8e7e933c1e56ee2e7b16a902d32bfa8
shamim reza <reza_cse08@hotmail.com>
2020-12-28 22:44:07 +0600

    changes

---
+++ Programming/LeetCode/DecemberPractics/TwoSumSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Programming.LeetCode.DecemberPractics
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var hash = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num2 = target - nums[i];
+                if (hash.ContainsKey(num2))
+                    return new int[] { i, hash[num2] };
+                hash.Add(nums[i], i);
+            }
+
+            throw new Exception("Not found!");
+        }
+    }
+}
