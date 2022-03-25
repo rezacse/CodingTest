e9847687f3d75d30165e6d2c8fbb19a53682b41a
shamim reza <reza_cse08@hotmail.com>
2022-03-26 03:50:56 +0400

    binary

--- CPP/algos/binary_search.cpp
+++ CPP/algos/binary_search.cpp
@@ -3,15 +3,12 @@
 using namespace std;
 
 int binarySearch(vector<int> v, int t) {
-    int l = 0, h = v.size() - 1, i = -1;
+    int l = 0, h = v.size() - 1, m = 0;
     while (l <= h) {
         int m = (l + h) / 2;
-        if (v[m] == t) {
-            i = m;
-            break;
-        }
-        if (t < v[m]) h = m - 1;
+        if (v[m] == t) return m;
+        else if (t < v[m]) h = m - 1;
         else l = m + 1;
     }
-    return i;
+    return -1;
 }
